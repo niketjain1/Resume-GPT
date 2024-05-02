70b7a6b74eb8fe37befb5c115c8ac4445f3f3d68
niketjain1 <niketj2000@gmail.com>
2024-05-02 19:12:57 +0530

    Added the chat component on the page.tsx file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+"use client";
+
+import { Chat } from "@/components/chat";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen p-8 bg-gray-700">
-      <div>Hello</div>
+    <main className="flex min-h-screen min-w-screen p-8 bg-gray-700">
+      <Chat />
     </main>
   );
 }
